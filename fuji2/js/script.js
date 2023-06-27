7920ace8f4c5829195b9b825bb07faa531c8faa3
OuraKouhei <135561193+OuraKouhei@users.noreply.github.com>
2023-06-27 20:10:17 +0900

    Add files via upload

--- fuji2/js/script.js
+++ fuji2/js/script.js
@@ -7,10 +7,57 @@ $(function () {
   $(window).scroll(function () {
     var scroll = $(this).scrollTop();
     var mvHeight = $(".mainvisual").height();
-    if(scroll > mvHeight){
-        $("header").addClass("scroll");
-    }else{
-        $("header").removeClass("scroll");
+    if (scroll > mvHeight) {
+      $("header").addClass("scroll");
+    } else {
+      $("header").removeClass("scroll");
     }
   });
+  $(window).scroll(function () {
+    fadeAnime();
+  });
+  fadeAnime();
 });
+
+function fadeAnime() {
+  $(".fadeUpTrigger").each(function () {
+    $(this).css({ opacity: "0" });
+    var obj = $(this).find("img");
+    var elemPos = $(this).offset().top + 100;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("fadeUp");
+      setTimeout(() => {
+        $(obj).css({ opacity: "1" });
+      }, 300);
+    }
+  });
+  $(".fadeRightTrigger").each(function () {
+    $(this).css({ opacity: "0" });
+    var elemPos = $(this).offset().top + 100;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("fadeRight");
+    }
+  });
+  $(".fadeDownTrigger").each(function () {
+    $(this).css({ opacity: "0" });
+    var elemPos = $(this).offset().top + 100;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("fadeDown");
+    }
+  });
+  $(".fadeLeftTrigger").each(function () {
+    $(this).css({ opacity: "0" });
+    var elemPos = $(this).offset().top + 100;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("fadeLeft");
+    }
+  });
+}
